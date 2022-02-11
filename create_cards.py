ef519d811287232fe46b90f7dc5d429672131b51
Mike MacHenry <mike.machenry@gmail.com>
2022-02-10 21:11:13 -0500

    automatically line break words

--- create_cards.py
+++ create_cards.py
@@ -10,6 +10,8 @@ black = (0,0,0)
 white = (255,255,255)
 font_file = 'easy-print.regular.ttf'
 output_dir = "cards"
+margin = 0.05
+marginal_width = width - width * margin * 2
 
 def main (filename):
   if not os.path.exists(output_dir):
@@ -19,24 +21,34 @@ def main (filename):
       create_card(line.rstrip())
 
 def create_card (word):
-  lines = word.split("|")
-  filename = re.sub("[|]", " ", word)
   font = ImageFont.truetype(font_file, max_font_size)
-  fit_font = fit_to_card (font, lines)
+  text = add_line_break(font, word)
+  fit_font = fit_to_card (font, text)
   img = Image.new('RGB', (width, height), color = white)
   draw = ImageDraw.Draw(img)
   draw.multiline_text(
     (width / 2, height / 2),
-    "\n".join(lines),
+    "\n".join(text),
     font = fit_font,
     fill = black,
     anchor = "mm",
     align = "center")
   #rot_img = img.rotate(270, fillcolor = white)
-  img.save(output_dir + "/" + filename + ".png")
+  img.save(output_dir + "/" + word + ".png")
+
+def add_line_break(font, word):
+  if font.getsize(word)[0] < marginal_width or ' ' not in word:
+    return [word]
+  else:
+    return split_at_middle_space(word)
+
+def split_at_middle_space(word):
+  spaces = [pos for pos, char in enumerate(word) if char == ' ']
+  spaces.sort(key=lambda p: abs(len(word)/2 - p))
+  middle = spaces[0]
+  return [word[0:middle], word[middle+1:]]
 
 def fit_to_card (font, lines):
-  marginal_width = width * 0.9
   size = max_font_size
   new_font = font
   while max([new_font.getsize(txt)[0] for txt in lines]) > marginal_width:
